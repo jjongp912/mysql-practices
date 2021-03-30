e9d1df6da022953cf63983a631f7e75b3f3112a8
jjongp912 <jjongp912@naver.com>
2021-03-30 11:57:56 +0900

    MySQL-practice

---
+++ sql-practices/basic-sql-practices.sql
@@ -0,0 +1,29 @@
+-- pet table 생성
+create table pets(
+name varchar(20),
+owner varchar(20),
+species varchar(20),
+gender char(1),
+birth date,
+death date
+);
+
+-- table scheme 확인
+desc pets;
+
+-- insert
+insert into pets values('보리','jjongp','dog','w','2019-06-21',null);
+insert into pets(owner,name,species,gender,birth)
+values('jjongp','coco','cat','m','2018-9-12');
+insert into pets values('마음이','jjongp','dog','m','2010-08-12','2020-12-23');
+
+-- select
+select * from pets;
+
+select name, birth from pets;
+
+select name, birth from pets order by birth asc;
+
+select count(*) from pets;
+select count(*) from pets where death is not null;
+
